f4534f73d66385af99895a20d984b2059251f08b
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-08-29 10:50:10 +0530

    Updated matrix vector multiplication

--- matrixVectorMultiplication.cpp
+++ matrixVectorMultiplication.cpp
@@ -18,31 +18,57 @@ int main(int argc, char **argv)
     if (rank == 0)
     {
         // initialize the matrix
+        int init_matrix[N][N] = {
+            {1, 2, 3, 4}, {7, 2, 3, 4}, {9, 1, 3, 5}, {1, 2, 9, 5}};
 
         // flatten the matrix
-
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                matrix_A[i * N + j] = init_matrix[i][j];
+            }
+        }
         // initialize the vector
+        vector_X = {1, 3, 4, 5};
 
         // print if you want
     }
 
     // scatter matrix A to all the processors
-    MPI_Scatter();
+    vector<int> local_matrix(N);
+    MPI_Scatter(&matrix_A, N, MPI_INT, &local_matrix, N, MPI_INT, 0, MPI_COMM_WORLD);
 
     // scatter vector X to all the processors
-    MPI_Scatter();
+    vector<int> local_vector(N / size);
+    MPI_Scatter(&vector_X, N / size, MPI_INT, &local_vector, N / size, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Gather the full vector on all the processors
-    MPI_Allgather();
+    vector<int> full_vector(N);
+    MPI_Allgather(&local_vector, N / size, MPI_INT, &full_vector, N / size, MPI_INT, MPI_COMM_WORLD);
 
     // computation on each processor
-
-    // Gather results at the rank 0
-    MPI_Gather();
+    vector<int> local_result_vec(N);
+    int local_result = 0;
+    // local_result[rank] = 0;
+    for (int i = 0; i < N; i++)
+    {
+        local_result += local_matrix[i] * full_vector[i];
+    }
+    local_result_vec[rank] = local_result;
+    // Gather results at the rank
+    vector<int> result(N);
+    MPI_Gather(&local_result, 1, MPI_INT, &result, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     // print the result
     if (rank == 0)
     {
+        cout << "Output :: " << endl;
+        for (auto ele : result)
+        {
+            cout << ele << " ";
+        }
+        cout << endl;
     }
 
     MPI_Finalize();
