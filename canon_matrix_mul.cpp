d184d44de9ac243466968f44c7ccff632992530b
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-11-05 12:40:03 +0530

    updated canon matrix multiplication

--- canon_matrix_mul.cpp
+++ canon_matrix_mul.cpp
@@ -33,3 +33,16 @@ void multiplyMatrices(vector<vector<double>> &A, vector<vector<double>> &B, vect
         }
     }
 }
+int main(int argc, char **argv)
+{
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int n = 8; // size of matrix
+
+    MPI_Finalize();
+    return 0;
+}
