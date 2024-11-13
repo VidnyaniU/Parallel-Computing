c98b95b1e0376dac1acabe8d33e4277eecaebb9e
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-11-13 08:44:19 +0530

    updated canon matrix

--- canon_matrix_mul.cpp
+++ canon_matrix_mul.cpp
@@ -1,92 +1,114 @@
-#include <bits/stdc++.h>
-#include <mpi.h>
-using namespace std;
+#include<mpi.h>
+#include<stdio.h>
+#include<string.h>
 
-vector<double> flattenMatrix(vector<vector<double>> &matrix)
+void main(int argc,char *argv[])
 {
-    int rows = matrix.size();
-    int cols = matrix[0].size();
-    vector<double> flatMatrix(rows * cols); // 1D vector to store the flattened matrix
+	MPI_Init(&argc,&argv);
+  	int dim=2;
+  	int dimv[2]={2,2};
+  	int period[2]={1,1};
+  	int reorder=1;
+  	MPI_Comm Cart_Topology;
+  	MPI_Cart_create(MPI_COMM_WORLD,dim,dimv,period,reorder,&Cart_Topology);
+	int rank;
+	MPI_Comm_rank(Cart_Topology,&rank);
+	int A[4][4],B[4][4],C[4][4], A_local[2][2], B_local[2][2], C_local[2][2];
+	if(rank==0)
+	{	
+		int A_temp[4][4] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+		int B_temp[4][4] = {16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+		int C_temp[4][4] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	 	memcpy(A, A_temp, sizeof(A_temp));
+		memcpy(B, B_temp, sizeof(B_temp));
+		memcpy(C, C_temp, sizeof(C_temp));
+	}
+	int sendcounts1[4] = {2,2,2,2};
+	int displs1[4] = {0,2,8,10};
+	MPI_Scatterv(&A, sendcounts1, displs1, MPI_INT, &A_local[0], 2, MPI_INT, 0, Cart_Topology);
+	MPI_Scatterv(&B, sendcounts1, displs1, MPI_INT, &B_local[0], 2, MPI_INT, 0, Cart_Topology);
+	int sendcounts2[4] = {2,2,2,2};
+	int displs2[4] = {4,6,12,14};
+	MPI_Scatterv(&A, sendcounts2, displs2, MPI_INT, &A_local[1], 2, MPI_INT, 0, Cart_Topology);
+	MPI_Scatterv(&B, sendcounts2, displs2, MPI_INT, &B_local[1], 2, MPI_INT, 0, Cart_Topology);
+	for(int step=0;step<2;step++)
+	{       if(step==0)
 
-    for (int i = 0; i < rows; ++i)
-    {
-        for (int j = 0; j < cols; ++j)
-        {
-            flatMatrix[i * cols + j] = matrix[i][j]; // Flatten row-wise
+		{if (rank==2||rank==3)
+		{
+		MPI_Request send_request, recv_request;
+		MPI_Status status;		
+		int left,right;
+  		int shift=-1;
+  		int change=1;
+ 		MPI_Cart_shift(Cart_Topology,change,shift,&left,&right);
+		MPI_Isend(&A_local,4,MPI_INT,left,0,Cart_Topology,&send_request);
+		MPI_Irecv(&A_local,4,MPI_INT,right,0,Cart_Topology,&recv_request);
+		MPI_Wait(&send_request, &status);
+		MPI_Wait(&recv_request, &status);
+		}	
+		if (rank==1||rank==3)
+		{
+		MPI_Request send_request1, recv_request1;
+		MPI_Status status1;		
+		int up,down;
+  		int shift=1;
+  		int change=0;
+ 		MPI_Cart_shift(Cart_Topology,change,shift,&up,&down);
+		MPI_Isend(&B_local,4,MPI_INT,up,1,Cart_Topology,&send_request1);
+		MPI_Irecv(&B_local,4,MPI_INT,down,1,Cart_Topology,&recv_request1);
+		MPI_Wait(&send_request1, &status1);
+		MPI_Wait(&recv_request1, &status1);
+		}
+		}
+	
+	if(step!=0)
+	    	{
+		MPI_Request send_request2, recv_request2;
+		MPI_Status status2;		
+		int left1,right1;
+  		int shift1=-1;
+  		int change1=1;
+ 		MPI_Cart_shift(Cart_Topology,change1,shift1,&left1,&right1);
+		MPI_Isend(&A_local,4,MPI_INT,left1,0,Cart_Topology,&send_request2);
+		MPI_Irecv(&A_local,4,MPI_INT,right1,0,Cart_Topology,&recv_request2);
+		MPI_Wait(&send_request2, &status2);
+		MPI_Wait(&recv_request2, &status2);
+
+		MPI_Request send_request3, recv_request3;
+		MPI_Status status3;		
+		int up1,down1;
+  		int shift2=1;
+  		int change2=0;
+ 		MPI_Cart_shift(Cart_Topology,change2,shift2,&up1,&down1);
+		MPI_Isend(&B_local,4,MPI_INT,up1,1,Cart_Topology,&send_request3);
+		MPI_Irecv(&B_local,4,MPI_INT,down1,1,Cart_Topology,&recv_request3);
+		MPI_Wait(&send_request3, &status3);
+		MPI_Wait(&recv_request3, &status3);
+		}
+
+	for(int i=0;i<2;i++)
+ 	   {	for (int j=0;j<2;j++)
+		{ for (int k=0;k<2;k++)
+			{C_local[i][j] =C_local[i][j]+ A_local[i][k]*B_local[k][j];}
+		}
+	   }
+	}
+
+	MPI_Gatherv(&C_local[0],2, MPI_INT,&C,sendcounts1, displs1, MPI_INT, 0, Cart_Topology);
+	MPI_Gatherv(&C_local[1],2, MPI_INT,&C,sendcounts2, displs2, MPI_INT, 0, Cart_Topology);
+
+
+	if (rank==0)
+	{
+		for(int i=0;i<4;i++)
+ 		{for (int j=0;j<4;j++)
+		{printf("%d ",C[i][j]);}
+		printf("\n");}
         }
-    }
+	
+	
 
-    return flatMatrix;
-}
-
-void multiplyMatrices(vector<vector<double>> &A, vector<vector<double>> &B, vector<vector<double>> &C, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            C[i][j] = 0; // Initialize C[i][j] to 0
-            for (int k = 0; k < n; k++)
-            {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-}
-int main(int argc, char **argv)
-{
-    int rank, size;
-
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    int n = 8; // size of matrix
-
-    vector<vector<double>> A(n, vector<double>(n)), B(n, vector<double>(n));
-    vector<vector<double>> final_C(n, vector<double>(n, 0)); // Initialize final result matrix
-    if (rank == 0)
-    {
-        ifstream fin("matrix_matrix_mul_input.txt");
-        if (!fin)
-        {
-            cerr << "Error opening file." << endl;
-            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-        }
-
-        // Read matrices A and B from file
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < n; ++j)
-                fin >> A[i][j];
-
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < n; ++j)
-                fin >> B[i][j];
-
-        fin.close();
-
-        if (size != 4)
-        {
-            cout << "Error: Number of processors should be 4." << endl;
-            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-        }
-    }
-    // Broadcast matrix B to all processes
-    MPI_Bcast(B.data(), n * n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    int block_size = n / sqrt(size);
-    vector<double> local_A(block_size * n);    // Each process will receive block_size rows
-    vector<double> local_C(block_size * n, 0); // Initialize local C
-    // Scatter rows of A to all processes
-    MPI_Scatter(A.data(), block_size * n, MPI_DOUBLE, local_A.data(), block_size * n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    // Convert local_A to a 2D vector for easier multiplication
-    vector<vector<double>> local_A_2D(block_size, vector<double>(n));
-    for (int i = 0; i < block_size; ++i)
-        for (int j = 0; j < n; ++j)
-            local_A_2D[i][j] = local_A[i * n + j];
-
-            // Perform local matrix multiplication
-    vector<vector<double>> local_C_2D(block_size, vector<double>(n));
-    multiplyMatrices(local_A_2D, B, local_C_2D, block_size);
-    MPI_Finalize();
-    return 0;
+	
+	MPI_Finalize();
 }
